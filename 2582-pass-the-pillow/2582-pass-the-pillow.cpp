a1790cf1fb06dfd56b0ae8b3c62c98ea3b62f988
Shalin Dashora <102173175+shalin-sd@users.noreply.github.com>
2023-03-06 22:06:07 +0530

    Time: 0 ms (100.00%), Space: 6 MB (100.00%) - LeetHub

---
+++ 2582-pass-the-pillow/2582-pass-the-pillow.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+                int  position = 1;
+    bool going_right = true;
+
+    for(int i=0;i<time;i++){
+        if(going_right)
+            position += 1;
+        else
+            position -= 1;
+
+        if(position == 1 || position == n)
+            going_right = !going_right;
+    }
+
+    return position;
+ 
+    }
+};
